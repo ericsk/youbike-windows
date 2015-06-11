ba5e4f181b51faceb7a529764a1ca77d6a54aa59
Eric ShangKuan <ericsk@gmail.com>
2015-06-11 23:54:59 +0800

    Show the Direction and Routes on the map control.

--- YouBikeUAP/YouBikeUAP.WindowsPhone/StationPage.xaml.cs
+++ YouBikeUAP/YouBikeUAP.WindowsPhone/StationPage.xaml.cs
@@ -3,6 +3,8 @@
   using System;
   using System.Diagnostics;
   using Windows.Devices.Geolocation;
+  using Windows.Services.Maps;
+  using Windows.UI;
   using Windows.UI.ViewManagement;
   using Windows.UI.Xaml;
   using Windows.UI.Xaml.Controls;
@@ -28,6 +30,7 @@
 
       DataContext = this;
       BingMapsKey = Constants.BING_MAPS_KEY;
+      MapService.ServiceToken = BingMapsKey;
 
       this.navigationHelper = new NavigationHelper(this);
       this.navigationHelper.LoadState += this.NavigationHelper_LoadState;
@@ -45,6 +48,7 @@
       StationName.Text = station.sna;
       BikeStopAvailable.Text = station.sbi;
       BikeStopCapacity.Text = station.bemp;
+      Address.Text = station.ar;
 
       var geopos = new Geopoint(new BasicGeoposition() { Latitude = station.lat, Longitude = station.lng });
 
@@ -54,7 +58,7 @@
       // add map icon
       MapIcon stationIcon = new MapIcon();
       stationIcon.Location = geopos;
-      stationIcon.Title = station.sna + "站";
+      stationIcon.Title = station.sna;
       stationMapControl.MapElements.Add(stationIcon);
 
     }
@@ -63,13 +67,55 @@
     {
     }
 
-    private async void OnDirectionsCommandClick(object sender, RoutedEventArgs e)
+    private async void OnDirectionsCommandClick(object sender, RoutedEventArgs args)
     {
-      await Windows.System.Launcher.LaunchUriAsync
-      (
-          new Uri(string.Format("ms-walk-to:?destination.latitude={0}&destination.longitude={1}&destination.name={2}",
-            station.lat, station.lng, station.sna), UriKind.Absolute)
-      );
+      // show the progress indicator
+      await statusBar.ShowAsync();
+      await progressIndicator.ShowAsync();
+
+      // retrive the user's location
+      var geolocator = new Geolocator();
+      geolocator.DesiredAccuracyInMeters = 50;
+
+      Geoposition geopos = null;
+      try
+      {
+        geopos = await geolocator.GetGeopositionAsync(
+             maximumAge: TimeSpan.FromMinutes(5),
+             timeout: TimeSpan.FromSeconds(10)
+            );
+      }
+      catch (Exception e)
+      {
+        Debug.WriteLine("[HubPage$LoadState] " + e.Message);
+      }
+
+      if (geopos != null)
+      {
+        var startPoint = geopos.Coordinate.Point;
+        var endPoint = new Geopoint(new BasicGeoposition() { Latitude = station.lat, Longitude = station.lng });
+
+        MapRouteFinderResult routeResult =
+                  await MapRouteFinder.GetWalkingRouteAsync(startPoint, endPoint);
+
+        if (routeResult.Status == MapRouteFinderStatus.Success)
+        {
+          // Use the route to initialize a MapRouteView.
+          MapRouteView viewOfRoute = new MapRouteView(routeResult.Route);
+          viewOfRoute.RouteColor = Colors.Yellow;
+          viewOfRoute.OutlineColor = Colors.Black;
+
+          // Add the new MapRouteView to the Routes collection
+          // of the MapControl.
+          stationMapControl.Routes.Add(viewOfRoute);
+
+          // Fit the MapControl to the route.
+          await stationMapControl.TrySetViewBoundsAsync(
+              routeResult.Route.BoundingBox,
+              null,
+              MapAnimationKind.None);
+        }
+      }
     }
 
 
